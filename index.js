26993712ed40fd3137e3766006fa8cfc45d79b38
HItmon <hitlabmodv2@gmail.com>
2025-05-05 12:00:46 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,153 @@
+const {
+  default: WAConnect,
+  useMultiFileAuthState,
+  DisconnectReason,
+  fetchLatestBaileysVersion,
+  makeInMemoryStore,
+  Browsers, 
+  fetchLatestWaWebVersion
+} = require("@whiskeysockets/baileys");
+const pino = require("pino");
+const readline = require('readline');
+const { Boom } = require("@hapi/boom");
+
+const pairingCode = process.argv.includes("--pairing-code");
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const question = (text) => new Promise((resolve) => rl.question(text, resolve));
+const store = makeInMemoryStore({ logger: pino().child({ level: "silent", stream: "store" }) });
+
+
+async function WAStart() {
+  const { state, saveCreds } = await useMultiFileAuthState("./sesi");
+  const { version, isLatest } = await fetchLatestWaWebVersion().catch(() => fetchLatestBaileysVersion());
+  console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
+
+  const client = WAConnect({
+    logger: pino({ level: "silent" }),
+    printQRInTerminal: !pairingCode,
+    browser: Browsers.ubuntu("Chrome"),
+    auth: state,
+    // Menambahkan konfigurasi untuk mengoptimalkan koneksi
+    version: version,
+    browserDescription: ["BOT", "Chrome", "3.0"],
+    connectTimeoutMs: 60000,
+    keepAliveIntervalMs: 10000,
+    defaultQueryTimeoutMs: 60000,
+    emitOwnEvents: true,
+    markOnlineOnConnect: false,
+    syncFullHistory: false,
+    qrTimeout: 40000,
+    retryRequestDelayMs: 250,
+    maxRetries: 5,
+    mobile: false,
+    patchMessageBeforeSending: false
+  });
+
+  store.bind(client.ev);
+
+  if (pairingCode && !client.authState.creds.registered) {
+    const phoneNumber = await question(`Silahkan masukin nomor Whatsapp kamu: `);
+    let code = await client.requestPairingCode(phoneNumber);
+    code = code?.match(/.{1,4}/g)?.join("-") || code;
+    console.log(`⚠︎ Kode Whatsapp kamu : ` + code)
+  }
+
+  client.ev.on("messages.upsert", async (chatUpdate) => {
+    try {
+      const m = chatUpdate.messages[0];
+      if (!m.message) return;
+      
+      const maxTime = 5 * 60 * 1000; // 5 menit
+
+      if (m.key && !m.key.fromMe && m.key.remoteJid === 'status@broadcast') {
+        if (!m.message.reactionMessage) {
+          const allowedSenders = [
+            "6281447345627@s.whatsapp.net",
+            "628145563553@s.whatsapp.net",
+          ];
+
+          if (!allowedSenders.includes(m.key.participant)) {
+            const currentTime = Date.now();
+            const messageTime = m.messageTimestamp * 1000;
+            const timeDiff = currentTime - messageTime;
+
+            if (timeDiff <= maxTime) {
+              // Emoji random untuk reaksi
+              const emojis = [
+                "🔥", "✨", "🤖", "🌟", "🌞", "🎉", "🎊", "😺"
+              ];
+
+              function getRandomEmoji() {
+                const randomIndex = Math.floor(Math.random() * emojis.length);
+                return emojis[randomIndex];
+              }
+
+              const randomEmoji = getRandomEmoji();
+              
+              try {
+                // Kirim reaksi emoji
+                await client.sendMessage("status@broadcast", {
+                  react: { text: randomEmoji, key: m.key },
+                }, { statusJidList: [m.key.participant] });
+
+                // Kirim like pada story
+                await client.sendMessage(m.key.remoteJid, {
+                  react: { text: "❤️", key: m.key }
+                });
+
+                // Tandai story sudah dibaca
+                await client.readMessages([m.key]);
+                console.log(`Berhasil melihat dan memberikan like pada status dari ${m.pushName}`);
+              } catch (error) {
+                console.error('Error:', error);
+              }
+            }
+          }
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+
+  client.ev.on("connection.update", async (update) => {
+    const { connection, lastDisconnect } = update;
+      if (connection === "close") {
+        let reason = new Boom(lastDisconnect?.error)?.output.statusCode;
+        if (reason === DisconnectReason.badSession) {
+          console.log(`Bad Session File, Please Delete Session and Scan Again`);
+          process.exit();
+        } else if (reason === DisconnectReason.connectionClosed) {
+          console.log("Connection closed, reconnecting....");
+          WAStart();
+        } else if (reason === DisconnectReason.connectionLost) {
+          console.log("Connection Lost from Server, reconnecting...");
+          WAStart();
+        } else if (reason === DisconnectReason.connectionReplaced) {
+          console.log("Connection Replaced, Another New Session Opened, Please Restart Bot");
+          process.exit();
+        } else if (reason === DisconnectReason.loggedOut) {
+          console.log(`Device Logged Out, Please Delete Folder Session and Scan Again.`);
+          process.exit();
+        } else if (reason === DisconnectReason.restartRequired) {
+          console.log("Restart Required, Restarting...");
+          WAStart();
+        } else if (reason === DisconnectReason.timedOut) {
+          console.log("Connection TimedOut, Reconnecting...");
+          WAStart();
+        } else {
+          console.log(`Unknown DisconnectReason: ${reason}|${connection}`);
+          WAStart();
+        }
+      } else if (connection === "open") {
+      console.log("Connected to Readsw");
+    }
+  });
+
+  client.ev.on("creds.update", saveCreds);
+
+  return client;
+}
+
+WAStart();
